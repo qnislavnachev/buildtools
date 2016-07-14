fd63fbc83cb82cfadc35fcc90195b04dc106f26c
clouway <alexandervladimirov1902@gmail.com>
2016-07-14 14:15:59 +0300

    Done With Gradle Project.

---
+++ MyProject/src/main/java/com/clouway/MyClass.java
@@ -0,0 +1,13 @@
+package com.clouway;
+
+/**
+ * Created by clouway on 14.07.16.
+ *
+ * @author Alexander Vladimirov
+ *         (alexandervladimirov1902@gmail.com)
+ */
+public class MyClass {
+  public static void main(String[] args) {
+    System.out.println("Hello Gradle");
+  }
+}
---
+++ MyProject/src/test/java/com/clouway/MainTest.java
@@ -0,0 +1,17 @@
+package com.clouway;
+
+import org.junit.Test;
+
+/**
+ * Created by clouway on 14.07.16.
+ *
+ * @author Alexander Vladimirov
+ *         (alexandervladimirov1902@gmail.com)
+ */
+public class MainTest {
+  @Test
+  public void main() throws Exception {
+
+  }
+
+}
