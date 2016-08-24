52c0862fe5c89a3d7861a35d51fc9b0489396213
qnislavnachgev <qnislav.nachev@gmail.com>
2016-08-24 14:39:41 +0300

    there are some changes on gradle task that we talk about

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
-        Logger log = Logger.getLogger("logger");
+        Logger log = Logger.getLogger(HelloWorld.class);
         log.info("Welcome to the system!");
 
     }
